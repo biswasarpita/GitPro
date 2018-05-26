d1c531f89658caa2f550acdf49349998fb7c4d74
Arpita <arpita.bt.cse@gmail.com>
2018-05-26 14:16:08 +0530

    Implemented config file

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+    getMessage: 'Successful GET',
+    displayMessage: 'Hello! I am here'
+}
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express');
-
+var config = require('./config');
 app = express();
 
 app.get('/v1/getMessage', function(req, res){
     res.send({
-        message: 'Successful GET'
+        message: config.getMessage
     })
-})
+});
 
 app.get('/v1/getTimestamp', function(req, res){
     var epochTimestamp = Date.now();
@@ -14,14 +14,14 @@ app.get('/v1/getTimestamp', function(req, res){
     res.send({
         message: timestamp
     })
-})
+});
 
 app.get('/v1/displayMessage', function(req, res){
     res.send({
-        message:'Hello!!! I am here'
+        message: config.displayMessage
     })
-})
+});
 
 app.listen(3000, function(){
     console.log('Listening on port 3000');
-})
+});
