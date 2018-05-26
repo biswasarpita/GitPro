ec4de2ccd5bb8fe1c315a9d430905590ce8dd4ce
Arpita <arpita.bt.cse@gmail.com>
2018-05-26 14:08:36 +0530

    Added dispalyMessage

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ app.get('/v1/getTimestamp', function(req, res){
     })
 })
 
+app.get('/v1/displayMessage', function(req, res){
+    res.send({
+        message:'Hello!!! I am here'
+    })
+})
+
 app.listen(3000, function(){
     console.log('Listening on port 3000');
 })
